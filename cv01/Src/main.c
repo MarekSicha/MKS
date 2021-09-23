8d7b864782e677df31d005e18d6ef3ca49c921b6
MarekSicha <mareceksicha@seznam.cz>
2021-09-23 11:52:57 +0200

    cv01/done

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -18,13 +18,45 @@
  */
 
 #include <stdint.h>
+#include <stm32f0xx.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+
 int main(void)
 {
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+	uint32_t sos = 0b10101001110111011100101010000000;
     /* Loop forever */
-	for(;;);
+	while(1)
+	{
+		/*
+		GPIOA->BSRR = (1<<5); // set
+		for (volatile uint32_t i = 0; i < 100000; i++) {}
+		GPIOA->BRR = (1<<5); // reset
+		for (volatile uint32_t i = 0; i < 100000; i++) {}
+		*/
+
+		for(uint32_t i = 0;i<32;i++)
+		{
+			if(sos & (1<<(31-i)))
+			{
+				GPIOA->BSRR = (1<<5); // set
+			}
+
+			else
+			{
+				GPIOA->BRR = (1<<5); // reset
+			}
+
+			for (volatile uint32_t i=0; i<100000;i++){}
+		}
+
+
+
+	}
 }
