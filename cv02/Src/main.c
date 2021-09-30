489e126fce8ae3271eaeaaecc4a89e2b3802b0d2
MarekSicha <mareceksicha@seznam.cz>
2021-09-30 12:01:10 +0200

    uk3

--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -20,13 +20,117 @@
 #include <stdint.h>
 #include "stm32f0xx.h"
 
+#define  LED_TIME_BLINK 300
+#define  LED_TIME_SHORT 100
+#define  LED_TIME_LONG  1000
+#define	 CYCLE			40
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
+
+volatile uint32_t Tick;
+
+void EXTI0_1_IRQHandler(void)
+{
+	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+		GPIOB->ODR ^= (1<<0); // toggle
+	}
+}
+
+void SysTick_Handler(void)
+{
+	Tick++;
+}
+
+void blikac(void)
+{
+	static uint32_t delay;
+
+
+	if (Tick > delay + LED_TIME_BLINK) {
+		GPIOA->ODR ^= (1<<4);
+		delay = Tick;
+	}
+}
+
+void tlacitka(void)
+{
+	static uint32_t old_s2;
+	static uint32_t old_s1;
+	static uint32_t off_time_s2;
+	static uint32_t off_time_s1;
+	static uint32_t delay_s2;
+	static uint32_t delay_s1;
+	static uint32_t sos = 0;
+
+
+	if (Tick > delay_s2 + CYCLE){
+
+		uint32_t new_s2 = GPIOC->IDR & (1<<0);  // read but 2
+
+		if (old_s2 && !new_s2) { // falling edge
+			off_time_s2 = Tick + LED_TIME_SHORT;
+			GPIOB->BSRR = (1<<0);
+		}
+		old_s2 = new_s2;
+
+
+		if (Tick > off_time_s2 && sos ==1) {
+			GPIOB->BRR = (1<<0);
+		}
+
+		delay_s2 = Tick;
+	}
+
+	if (Tick > delay_s1 + CYCLE){
+
+		uint32_t new_s1 = GPIOC->IDR & (1<<1);  // read but 1
+
+		if (old_s1 && !new_s1) { // falling edge
+			off_time_s1 = Tick + LED_TIME_LONG;
+			GPIOB->BSRR = (1<<0);
+			sos = 0;
+		}
+		old_s1 = new_s1;
+
+
+		if (Tick > off_time_s1) {
+			GPIOB->BRR = (1<<0);
+			sos =1;
+		}
+
+		delay_s1 = Tick;
+	}
+}
+
 
 
 int main(void)
 {
 
+
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
+	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
+	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
+
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; // enable clock SYSCFG
+
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	EXTI->IMR |= EXTI_IMR_MR0; // mask
+	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	//NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+
+	SysTick_Config(8000); // 1ms
+
+	while (1)
+	{
+		blikac();
+		tlacitka();
+	}
 
 }
